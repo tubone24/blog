e718720bde56f1efcb44e76ffb4d81536623a652
tubone <meitantei-conan@live.jp>
2019-08-30 08:02:00 +0900

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -87,13 +87,6 @@ export const IndexPageTemplate = ({
                   </div>
                 </div>
                 <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
                     Latest stories
