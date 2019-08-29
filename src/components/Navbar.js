6dd7025465fe785d0d0d5bc13a6136079dc9490c
tubone <meitantei-conan@live.jp>
2019-08-30 01:24:41 +0900

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -60,26 +60,14 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
+                href="https://github.com/tubone24/blog"
                 target="_blank"
                 rel="noopener noreferrer"
               >
