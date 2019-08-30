164d9dba357d9d4bf1497aa1bc21aa10b31ae97d
Yu Otsubo <uu-ootsubo@kddi.com>
2019-08-30 18:13:13 +0900

    fix title

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'tubone-blog',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'tubone-blog',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
