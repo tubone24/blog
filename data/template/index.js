e8b4ad6b1370609e5bd70e8acf002f0de86fcf6b
tubone <meitantei-conan@live.jp>
2019-09-01 00:18:54 +0900

    Update index.js

--- data/template/index.js
+++ data/template/index.js
@@ -1,5 +1,10 @@
 const config = require('./config');
 
+const githubClientId = process.env.GITHUB_CLIENT_ID;
+const githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+
 module.exports = {
   config,
+  githubClientId,
+  githubClientSecret,
 };
