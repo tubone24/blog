9a287998267508e9ddaaa14f86fc39e3aa02853b
tubone24 <tubo.yyyuuu@gmail.com>
2019-09-21 02:29:59 +0900

    vendors

--- src/html.js
+++ src/html.js
@@ -31,25 +31,19 @@ const HTML = ({
       />
       {postBodyComponents}
       <script
-        src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossOrigin="anonymous"
+        src="vendors/js/jquery-3.2.1.slim.min.js"
       />
       <script
-        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossOrigin="anonymous"
+        src="vendors/js/popper.min.js"
       />
       <script
-        src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossOrigin="anonymous"
+        src="vendors/js/bootstrap.min.js"
       />
       <link
         rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.1/jquery.fancybox.min.css"
+        href="vendors/css/jquery.fancybox.min.css"
       />
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.1/jquery.fancybox.min.js" />
+      <script src="vendors/js/jquery.fancybox.min.js" />
     </body>
   </html>
 );
