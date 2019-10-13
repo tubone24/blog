ba7ddbcc017c3f3f73a8c3589e178fd82616182f
tubone24 <tubo.yyyuuu@gmail.com>
2019-10-13 23:10:28 +0900

    wip

---
+++ src/components/image-loader/index.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+if (typeof window !== 'undefined') {
+  require('intersection-observer');
+}
+
+import './styles.scss';
+
+export default class ImageLoader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+      doneAnimating: false
+    };
+
+    this.loadImage = this.loadImage.bind(this);
+  }
+
+  loadImage() {
+    const image = new Image();
+    image.src = this.props.img;
+    image.onload = () => {
+      this.setState({ loaded: true }, () => {
+        this.img.addEventListener(
+          "animationend",
+          () => {
+            this.setState({ doneAnimating: true });
+            this.props.loadingCallback();
+          },
+          false
+        );
+      });
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.lazyload) {
+      this.observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          const { isIntersecting, intersectionRatio } = entry;
+          if (isIntersecting === true || intersectionRatio > 0) {
+            this.loadImage();
+            this.observer.disconnect();
+            this.observer = null;
+          }
+        });
+      });
+      this.observer.observe(this.imageLoader);
+    } else {
+      this.loadImage();
+    }
+  }
+
+  componentWillUnmount() {
+    this.observer && this.observer.disconnect();
+  }
+
+  render() {
+    const showPlaceholder = this.props.placeholder && !this.state.doneAnimating;
+    return (
+      <div
+        ref={node => (this.imageLoader = node)}
+        className={classNames("image-loader", {
+          [`${this.props.className}`]: this.props.className,
+          "image-loader--placeholder": this.props.aspectRatio
+        })}
+        style={this.props.styles}
+      >
+        {showPlaceholder && (
+          <img
+            className={classNames("placeholder", {
+              "fade-out": this.state.loaded
+            })}
+            src={this.props.placeholder}
+            alt=""
+          />
+        )}
+        {this.props.aspectRatio && (
+          <div style={{ paddingBottom: `${this.props.aspectRatio * 100}%` }} />
+        )}
+        {this.state.loaded && (
+          <img
+            ref={node => (this.img = node)}
+            className="img"
+            src={this.props.img}
+            {...this.props.imageProps}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+ImageLoader.defaultProps = {
+  lazyload: true,
+  loadingCallback: () => {}
+};
+
+ImageLoader.propTypes = {
+  aspectRatio: PropTypes.number,
+  className: PropTypes.string,
+  img: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  lazyload: PropTypes.bool,
+  loadingCallback: PropTypes.func,
+  styles: PropTypes.object,
+  imageProps: PropTypes.object
+};
---
+++ src/components/navigation/index.js
@@ -0,0 +1,141 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import Link from "gatsby-link";
+import BodyClassName from "react-body-classname";
+import Helmet from "react-helmet";
+
+import "./styles.scss";
+
+export default class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.navItems = ["index", "about", "resume", "portfolio"]; // @todo: generate links based off of these.
+    this.state = {
+      path: this.getPath(props.location.pathname),
+      pageTitle: this.getPageTitle(this.props),
+      showNav: false
+    };
+  }
+
+  handleCloseClick = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({ showNav: false });
+  };
+
+  // @todo: make this generic.
+  handlePortfolioClick = e => {
+    if (!this.state.showNav && this.state.path === "portfolio") {
+      e.preventDefault();
+      this.setState({ showNav: true });
+    }
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const locationChanged =
+      prevProps.location.pathname !== this.props.location.pathname;
+    if (this.state.showNav === prevState.showNav) {
+      this.setState({
+        path: this.getPath(this.props.location.pathname),
+        pageTitle: this.getPageTitle(this.props),
+        showNav: !!(!this.state.showNav && !locationChanged)
+      });
+    }
+
+    if (locationChanged && window.analytics) {
+      window.analytics.page();
+    }
+  }
+
+  componentDidMount() {
+    // Page load
+    if (window.analytics) {
+      window.analytics.page();
+    }
+  }
+
+  getPath(path) {
+    return path === "/" ? "index" : path.split("/")[1].replace(/\//g, " ");
+  }
+
+  getPageTitle(props) {
+    return props.title;
+  }
+
+  render() {
+    const showSubNav =
+      this.state.path === "portfolio" && this.state.pageTitle !== "Portfolio";
+    return (
+      <BodyClassName
+        className={classNames(this.state.path, {
+          "show-nav": this.state.showNav
+        })}
+      >
+        <nav
+          className={classNames("main-nav", this.state.path, {
+            "show-nav": this.state.showNav
+          })}
+        >
+          <Helmet>
+            <html
+              lang="en"
+              className={classNames(this.state.path, {
+                "show-nav": this.state.showNav
+              })}
+            />
+          </Helmet>
+          <Link
+            to="/portfolio/"
+            onClick={this.handlePortfolioClick}
+            activeClassName="active"
+            className={classNames("nav-item", {
+              active: this.state.path === "portfolio"
+            })}
+          >
+            <span className="text">
+              <span
+                className={classNames("subnav-wrapper", {
+                  "has-subnav":
+                    this.state.path === "portfolio" &&
+                    this.state.pageTitle !== "Portfolio"
+                })}
+              >
+                <span className="subnav">
+                  <span className="subnav-text section-title">
+                    {"Portfolio"}
+                  </span>
+                </span>
+                <span className="subnav">
+                  {showSubNav && (
+                    <span className="subnav-text page-title">
+                      {this.state.pageTitle}
+                    </span>
+                  )}
+                </span>
+              </span>
+            </span>
+          </Link>
+          <Link to="/about/" activeClassName="active" className="nav-item">
+            <span className="text">{"About"}</span>
+          </Link>
+          <Link to="/resume/" activeClassName="active" className="nav-item">
+            <span className="text">{"Resume"}</span>
+          </Link>
+          <span className="nav-item close" onClick={this.handleCloseClick}>
+            <span className="close-btn" />
+          </span>
+        </nav>
+      </BodyClassName>
+    );
+  }
+}
+
+Navigation.defaultProps = {
+  location: {}
+};
+
+Navigation.propTypes = {
+  location: PropTypes.object.isRequired,
+  title: PropTypes.string
+};
