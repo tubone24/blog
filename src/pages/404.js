7d0ada2252c2579ffb80a46a05138e381d698474
tubone24 <tubo.yyyuuu@gmail.com>
2020-01-05 21:14:06 +0900

    fix 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,15 +2,18 @@
 import React from 'react';
 
 import { Link, graphql } from 'gatsby';
-import { Profile, Repository } from 'react-github-info';
 
 
 const NotFoundPage = ({ data }) => (
   <div className="container">
     <div className="row">
       <div className="col">
-        <Profile username="wonism" />
-        <Repository username="wonism" repos="til" />
+          <h4>404 Not Found...</h4>
+          {data.allSitePage.edges.map(page => (
+              <Link to={page.node.path} href={page.node.path} key={page.node.path}>
+                  <li>{page.node.path}</li>
+              </Link>
+          ))}
       </div>
     </div>
   </div>
