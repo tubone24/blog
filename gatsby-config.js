bc9ef3c107fc6539e8cef221670694d0a4cd9169
tubone24 <tubo.yyyuuu@gmail.com>
2020-03-14 15:09:07 +0900

    delete html tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -111,7 +111,7 @@ module.exports = {
               date: edge.node.frontmatter.date,
               url: `${site.siteMetadata.siteUrl}/${edge.node.fields.slug}`,
               guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-              custom_elements: [{ 'content:encoded': (edge.node.html).substr(0, 100) }],
+              custom_elements: [{ 'content:encoded': edge.node.html.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '').substr(0, 150) }],
             })),
             query: `
               {
