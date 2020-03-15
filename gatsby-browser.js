cd9d9688c3dff7a9474e85dbae1b08b8e0e552c8
tubone24 <tubo.yyyuuu@gmail.com>
2020-03-15 10:56:22 +0900

    detect update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -19,10 +19,10 @@ installFontAwesome();
 
 const isLocalDevelopment = () => window && window.location && window.location.origin !== url;
 
-window.addEventListener('lazybeforeunveil', function(e){
-  var bg = e.target.getAttribute('data-bg');
-  if(bg){
-    e.target.style.backgroundImage = 'url(' + bg + ')';
+window.addEventListener('lazybeforeunveil', (e) => {
+  const bg = e.target.getAttribute('data-bg');
+  if (bg) {
+    e.target.style.backgroundImage = `url(${bg})`;
   }
 });
 
