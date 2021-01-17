33da522993b4add944db3d05c5b109154cd71f4f
tubone24 <tubo.yyyuuu@gmail.com>
2021-01-17 16:56:57 +0900

    :horse: use CDN

--- src/html.js
+++ src/html.js
@@ -19,7 +19,6 @@ const HTML = ({
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
       <meta name="p:domain_verify" content="2accdaac3153e1b438ab307ac5c4b855" />
-      <link rel="preconnect" href="https://ajax.aspnetcdn.com/" crossOrigin />
       <link
         rel="stylesheet"
         href="https://ajax.aspnetcdn.com/ajax/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossOrigin="anonymous"
@@ -40,8 +39,8 @@ const HTML = ({
         dangerouslySetInnerHTML={{ __html: body }}
       />
       {postBodyComponents}
-      <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossOrigin="anonymous" />
-      <script src="https://ajax.aspnetcdn.com/ajax/bootstrap/4.5.2/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossOrigin="anonymous" />
+      <script src="/vendors/js/jquery-3.5.1.slim.min.js" />
+      <script src="/vendors/js/bootstrap.min.js" />
       <script src="/vendors/js/lazysizes.min.js" async="" />
     </body>
   </html>
