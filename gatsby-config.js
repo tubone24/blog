9c4d5f9d1faa7d12f4e5c466fe30583ab24fbc79
tubone <tubo.yyyuuu@gmail.com>
2021-02-18 02:13:10 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,6 +103,7 @@ module.exports = {
             // eslint-disable-next-line max-len
             serialize: ({ query: { site, allMarkdownRemark } }) => allMarkdownRemark.edges.map((edge) => ({
               // ...edge.node.frontmatter,
+              title: edge.node.title,
               description: edge.node.excerpt.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '').replace(/\s+/g, '').replace(/#x.*;/, '').replace(/&/, ''),
               date: edge.node.frontmatter.date,
               url: `${site.siteMetadata.siteUrl}/${edge.node.fields.slug}`,
