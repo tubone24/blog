0271ae9a10d336a805f73b68e88f26be800c610c
tubone <meitantei-conan@live.jp>
2021-02-20 08:30:42 +0000

    search all text

--- gatsby-plugin-algolia-config.js
+++ gatsby-plugin-algolia-config.js
@@ -34,7 +34,7 @@ const queries = [
       }) => ({
         title,
         description: excerpt,
-        allText: html.replace(/<code[\s, \S]*?<\/code>/g, '').replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '').slice(0, 4000),
+        allText: html.replace(/<code[\s, \S]*?<\/code>/g, '').replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '').slice(0, 3500),
         tags: tags.join(),
         path: slug,
       }),
