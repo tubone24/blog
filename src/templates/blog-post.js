05abf4d2e38cb11bb7a50bb060555d5b02e294b2
tubone24 <tubo.yyyuuu@gmail.com>
2021-02-21 21:24:18 +0900

    event check

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -39,6 +39,11 @@ class BlogPost extends Component {
     const clientId = process.env.GATSBY_GITHUB_CLIENT_ID;
     const clientSecret = process.env.GATSBY_GITHUB_CLIENT_SECRET;
     const GitTalkInstance = new Gitalk({
+      repo: 'blog',
+      admin: ['tubone24'],
+      owner: 'tubone24',
+      pagerDirection: 'first',
+      distractionFreeMode: true,
       clientID: clientId,
       clientSecret,
       title,
