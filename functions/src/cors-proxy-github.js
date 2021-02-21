34e34fe22303e1ff78078d9fc587c0f4ca767fff
tubone24 <tubo.yyyuuu@gmail.com>
2021-02-21 21:36:07 +0900

    event check

--- functions/src/cors-proxy-github.js
+++ functions/src/cors-proxy-github.js
@@ -6,7 +6,7 @@ exports.handler = (event, context) => {
     accept: 'application/json',
   };
   return axios.post('https://github.com/login/oauth/access_token', body, {headers: headers}).then((response) => {
-    console.log(response.data)
+    console.log({access_token: response.data.access_token.substring(0, 3) + 'xxxxxxxxxxxxxxxxx', token_type: response.data.token_type, scope: response.data.scope})
     return {
       statusCode: 200,
       body: JSON.stringify({access_token: response.data.access_token, token_type: response.data.token_type, scope: response.data.scope}),
