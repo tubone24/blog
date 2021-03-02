5fc0b2eb2a17b5b671b9b05c7ebea958adf4f423
tubone <tubo.yyyuuu@gmail.com>
2021-03-03 02:06:05 +0900

    Update html.js

--- src/html.js
+++ src/html.js
@@ -24,8 +24,8 @@ const HTML = ({
         href="/vendors/css/bootstrap.min.custom.css"
       />
       <link rel="prefetch" href="/fonts/icomoon.woff2" />
-      <link rel="preconnect dns-prefetch" href="//res.cloudinary.com" />
-      <link rel="dns-prefetch" href="//sentry.io" />
+      <link rel="preconnect dns-prefetch" href="//sentry.io" />
+      <link rel="preconnect dns-prefetch" href="//i.imgur.com" />
       {headComponents}
     </head>
     <body {...bodyAttributes}>
