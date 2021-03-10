2b27903559d23ebc9aa0aec0c652ab0274a42d07
tubone <tubo.yyyuuu@gmail.com>
2021-03-11 01:10:43 +0900

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,7 +9,7 @@ import './src/styles/global.scss';
 
 Sentry.init({
   dsn: 'https://097c36a02dd64e139ba2952e8882046d@sentry.io/1730608',
-  release: `tubone-boyaki@${process.env.COMMIT_REF}`,
+  release: `tubone-boyaki@${process.env.GITHUB_SHA}`,
   integrations: [new Integrations.BrowserTracing()],
   tracesSampleRate: 1.0,
 });
