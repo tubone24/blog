f21391ab8a0bb5e59c30c63dc57e5912a46543a0
tubone24 <tubo.yyyuuu@gmail.com>
2021-04-25 11:18:10 +0900

    cypress UI regression test

--- cypress/integration/e2e/main.spec.js
+++ cypress/integration/e2e/main.spec.js
@@ -1,8 +1,16 @@
 describe("UI Test", () => {
-  it('compare ', () => {
+  it('compare toppage', () => {
     cy.visit('/');
     cy.wait(5000);
     cy.compareSnapshot('top', {
+      capture: 'fullPage',
+      errorThreshold: 0.1
+    });
+  });
+  it('compare privacy policy', () => {
+    cy.visit('/privacy-policies');
+    cy.wait(5000);
+    cy.compareSnapshot('privacy-policies', {
       capture: 'fullPage',
       errorThreshold: 0.1
     });
