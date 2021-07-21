0f1924e394206d1a106ad1b640e12adae4f82273
tubone24 <tubo.yyyuuu@gmail.com>
2021-07-21 15:04:42 +0900

    adstir

--- static/adstir.js
+++ static/adstir.js
@@ -1,9 +1,6 @@
-var adstirScript = document.createElement('script');
-adstirScript.src = "https://js.ad-stir.com/js/adstir.js";
 var adstir_vars = {
   ver: "4.0",
   app_id: "MEDIA-b223d6ca",
   ad_spot: 1,
   center: false
 };
-document.body.appendChild(adstirScript);
