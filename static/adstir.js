07900ae174c2ea6e3724f5c701f4a9a962c72683
tubone(Yu Otsubo) <tubo.yyyuuu@gmail.com>
2021-08-09 02:14:20 +0900

    minify

--- static/adstir.js
+++ static/adstir.js
@@ -1,10 +1,1 @@
-var adstir_vars = {
-  ver: "4.0",
-  app_id: "MEDIA-b223d6ca",
-  ad_spot: 1,
-  center: false
-};
-
-// var script = document.createElement('script');
-// script.src = "https://js.ad-stir.com/js/adstir.js";
-// document.body.appendChild(script);
+var adstir_vars={ver:"4.0",app_id:"MEDIA-b223d6ca",ad_spot:1,center:false};
