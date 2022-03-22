842c125b8b074a4f20b71fb126c93d70dbf1cfa0
tubone24 <tubo.yyyuuu@gmail.com>
2022-03-22 23:52:50 +0900

    wordcloud

--- scripts/wordcloud/src/main.py
+++ scripts/wordcloud/src/main.py
@@ -13,9 +13,9 @@ def main():
     exclude_list = generate_exclude_list()
     web = Web(BASE_URL, WEB_EXCLUDE_LIST)
     wordcloud_blog_words = WC(web.get_text_by_base_url(), exclude_list)
-    wordcloud_blog_words.generate_word_cloud("word_cloud_blog.png", alpha=False, mask="rect")
-    wordcloud_blog_words.generate_word_cloud("word_cloud_blog_large.png", alpha=True, mask="rect_large")
-    conv_webp("word_cloud_blog.png")
+    wordcloud_blog_words.generate_word_cloud("word-cloud-blog.png", alpha=False, mask="rect")
+    wordcloud_blog_words.generate_word_cloud("word-cloud-blog-large.png", alpha=True, mask="rect_large")
+    conv_webp("word-cloud-blog.png")
 
 
 if __name__ == "__main__":
