26030a0b9698c2021ad25e6a0175b6fa0f4b2c61
tubone24 <tubo.yyyuuu@gmail.com>
2022-04-30 22:21:43 +0900

    WIP

---
+++ src/components/Tag/index.stories.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Tag from "./index";
+
+export default {
+  title: "Tag",
+  component: Tag,
+  parameters: {
+    backgrounds: {
+      default: "green",
+      values: [{ name: "green", value: "#d5ffd7" }],
+    },
+  },
+} as ComponentMeta<typeof Tag>;
+
+const Template: ComponentStory<typeof Tag> = (args) => <Tag {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  name: "test",
+  count: 4,
+};
