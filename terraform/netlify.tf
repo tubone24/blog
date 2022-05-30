e3d6667df93040d9f26f868dfc700ab2ce4476d4
tubone24 <tubo.yyyuuu@gmail.com>
2022-05-31 02:13:20 +0900

    change provider

--- terraform/netlify.tf
+++ terraform/netlify.tf
@@ -9,5 +9,4 @@ provider "netlify" {
 resource "netlify_site" "main" {
   name = "pensive-lamport-5822d2"
   custom_domain = "blog.tubone-project24.xyz"
-  site_id = "3751ef40-b145-4249-9657-39d3fb04ae81"
 }
