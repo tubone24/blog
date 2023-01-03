7cad1876e35d5a38d8554a6b02f6bf262b6ca0fd
yu.otsubo <yu.otsubo@eventhub.jp>
2023-01-03 21:53:32 +0900

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -302,8 +302,9 @@ module.exports = {
         mergeDefaultDirectives: true,
         directives: {
           "script-src":
-            "'self' https://*.google-analytics.com https://*.twitter.com https://*.instagram.com https://embedr.flickr.com https://embed.redditmedia.com https://*.ad-stir.com https://blog-storybook.netlify.app https://www.youtube.com 'sha256-DSMO0z2RhMbkqQAuMaTTaJ19AmO447WUgmBfvurIXDc='", // sha-256はBootstrapのInline Script
+            "'self' https://*.google-analytics.com https://*.twitter.com https://*.instagram.com https://embedr.flickr.com https://embed.redditmedia.com https://*.ad-stir.com https://blog-storybook.netlify.app https://www.youtube.com https://widgets.flickr.com 'sha256-DSMO0z2RhMbkqQAuMaTTaJ19AmO447WUgmBfvurIXDc='", // sha-256はBootstrapのInline Script
           "style-src": "'self' 'unsafe-inline'",
+          "content-src": "'self' https://api.github.com",
           "img-src": "'self' data: *",
           "frame-ancestors":
             "'self' https://*.google-analytics.com https://*.twitter.com https://www.instagram.com https://embedr.flickr.com https://embed.redditmedia.com https://*.ad-stir.com https://blog-storybook.netlify.app https://www.youtube.com",
