c066e550e651068844b3dbcfd659fa1d62e2820c
yu.otsubo <yu.otsubo@eventhub.jp>
2023-01-13 01:12:35 +0900

    delete file

--- scripts/uploadScreenShot.ts
+++ scripts/uploadScreenShot.ts
@@ -1,4 +1,5 @@
-import { encode } from "https://deno.land/std/encoding/base64.ts";
+import { encode } from "https://deno.land/std@0.171.0/encoding/base64.ts";
+import { sleep } from "https://deno.land/x/sleep/mod.ts";
 
 const GITHUB_API_URL = "https://api.github.com";
 const AUTHOR_NAME = "tubone24";
@@ -13,10 +14,46 @@ const branchName = Deno.env.get("BRANCH_NAME") as string;
 const readImageData = await Deno.readFile(filePath);
 const encodedData = encode(readImageData);
 
-const message = prNumber === "" ? "[file upload] Added file on master" : `[file upload] Added file for PR #${prNumber}`;
+const uploadMessage = prNumber === "" ? "[file upload] Added file on master" : `[file upload] Added file for PR #${prNumber}`;
 
-const gitHubPayload = {
-  message: message,
+const deleteMessage = prNumber === "" ? "[file upload] Delete file on master" : `[file upload] Delete file for PR #${prNumber}`;
+
+const gitHubHeaders = {
+  Accept: "application/vnd.github.v3+json",
+  Authorization: `Bearer ${gitHubToken}`,
+};
+
+const content = await fetch(`${GITHUB_API_URL}/repos/${gitHubRepo}/contents/docs/screenshot/${fileName}?ref=${branchName}`, {
+  method: "GET",
+  headers: gitHubHeaders,
+});
+
+if (content.ok) {
+  const contentJson = await content.json();
+  console.log(contentJson.sha)
+  const gitHubDeletePayload = {
+    message: deleteMessage,
+    sha: contentJson.sha,
+    branch: branchName,
+    author: {
+      name: AUTHOR_NAME,
+      email: AUTHOR_EMAIL,
+    },
+    committer: {
+      name: AUTHOR_NAME,
+      email: AUTHOR_EMAIL,
+    },
+  }
+  await fetch(`${GITHUB_API_URL}/repos/${gitHubRepo}/contents/docs/screenshot/${fileName}`, {
+    method: "DELETE",
+    headers: gitHubHeaders,
+  });
+}
+
+await sleep(10);
+
+const gitHubUploadPayload = {
+  message: uploadMessage,
   content: encodedData.replace(new RegExp("data.*base64,"), ""),
   branch: branchName,
   author: {
@@ -29,17 +66,12 @@ const gitHubPayload = {
   },
 };
 
-const gitHubHeaders = {
-  Accept: "application/vnd.github.v3+json",
-  Authorization: `Bearer ${gitHubToken}`,
-};
-
 const gitHubUploadurl = `${GITHUB_API_URL}/repos/${gitHubRepo}/contents/docs/screenshot/${fileName}`;
 
 const gitHubRes = await fetch(gitHubUploadurl, {
   method: "PUT",
   headers: gitHubHeaders,
-  body: JSON.stringify(gitHubPayload),
+  body: JSON.stringify(gitHubUploadPayload),
 });
 
 console.log(gitHubRes);
