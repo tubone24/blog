f68cbd27c8ea13836eb3b7832c2f53e025e021dd
tubone <tubo.yyyuuu@gmail.com>
2023-03-21 18:34:45 +0900

    update

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   screenshotsFolder: "./cypress/snapshots/actual",
   trashAssetsBeforeRuns: true,
   chromeWebSecurity: false,
+  videoCompression: 15,
 
   e2e: {
     // We've imported your old cypress plugins here.
