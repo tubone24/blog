460a6f5d39f48eed26b4a715a7e487a41de296a5
tubone <tubo.yyyuuu@gmail.com>
2025-11-11 09:58:28 +0900

    refactor(ui): コメント機能を削除し、シェアボックスを簡素化

--- cypress/e2e/e2e/main.cy.ts
+++ cypress/e2e/e2e/main.cy.ts
@@ -11,9 +11,6 @@ describe("UI Test", () => {
   it("about page with smooth scroll", () => {
     cy.visit("/about");
     cy.location("href").should("include", "/about");
-    // eslint-disable-next-line cypress/no-unnecessary-waiting,testing-library/await-async-utils
-    cy.wait(1000);
-    cy.window().its("scrollY").should("equal", 0);
     cy.get("div > #toc_container > .toc_list > li").should(($lis) => {
       expect($lis, "toc list num").to.have.length(2);
       expect($lis.eq(0), "このブログについて").to.contain(
@@ -21,8 +18,9 @@ describe("UI Test", () => {
       );
       expect($lis.eq(1), "WordCloud").to.contain("2 WordCloud");
     });
+    cy.window().its("scrollY").should("equal", 0);
     cy.get("div > #toc_container > .toc_list > li:nth-child(1) > a").click();
-    cy.location("href").should("include", "/about/#aboutblog");
+    cy.location("href").should("include", "/about#aboutblog");
     cy.window().its("scrollY").should("not.equal", 0);
     cy.get("[data-testid=share-box] [data-testid=GotoTopButton]").click();
     // Because of smooth scrolling
