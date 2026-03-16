ebccbee92ca47279a06b91e1a2d3f9076c147f09
tubone <tubo.yyyuuu@gmail.com>
2026-03-16 10:55:06 +0900

    style(scss): @import文をurl()形式に統一

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,12 +1,15 @@
-module.exports = {
+export default {
   extends: [
     "stylelint-config-recommended-scss",
     "stylelint-config-standard",
+    "@stylistic/stylelint-config",
     "stylelint-config-recess-order",
   ],
   plugins: ["stylelint-scss"],
   rules: {
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
+    "declaration-property-value-no-unknown": null,
+    "nesting-selector-no-missing-scoping-root": null,
   },
 };
